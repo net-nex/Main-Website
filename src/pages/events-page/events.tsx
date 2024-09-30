637acf32936461eef1f8340d6a4eabc4b24ea012
Lohit Kolluri <lohitkolluri@gmail.com>
2024-10-01 00:59:37 +0530

    Date Update

--- src/pages/events-page/events.tsx
+++ src/pages/events-page/events.tsx
@@ -22,7 +22,7 @@ const Events = (props: Props) => {
           {/* Event Info */}
           <div className='md:sticky w-full md:w-[50vw] top-10 flex flex-col items-center justify-start text-center text-3xl'>
             <div className='text-3xl w-[90vw] pt-9 font-Lexend'>
-              <Fade duration={700}>Recruitment 2024</Fade>
+              <Fade duration={700}>Recruitments 2024</Fade>
             </div>
             <Fade duration={700}>
               <img src={`${server}`} alt='event' className='px-9 ' />
@@ -43,7 +43,7 @@ const Events = (props: Props) => {
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5" />
                   </svg>
-                  Oct 6th 2024
+                  Oct 1st 2024
                 </div>
               </div>
             </Fade>
