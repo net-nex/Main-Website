12e5ff0fe56f27f4a94f6dbc5ff0933f73450086
Lohit Kolluri <lohitkolluri@gmail.com>
2024-10-02 17:41:05 +0530

    Fix recruitments route

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homepage from './pages/home-page/homepage';
+import { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Events from './pages/events-page/events';
+import Homepage from './pages/home-page/homepage';
+import Induction from "./pages/ind-page";
 import Projects from './pages/projects/projects';
 import Registeration from "./pages/reg-page";
-import Induction from "./pages/ind-page";
+
+function RecruitmentsRedirect() {
+  useEffect(() => {
+    window.location.href = "https://forms.gle/gjn8S8VvPx7QvCK36";
+  }, []);
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Homepage/>} />
+          <Route path="/" element={<Homepage />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/events" element={<Events />} />
           <Route path="/reg" element={<Registeration />} />
           <Route path="/induction" element={<Induction />} />
+          <Route path="/recruitments" element={<RecruitmentsRedirect />} />
       </Routes>
     </BrowserRouter>
   );
